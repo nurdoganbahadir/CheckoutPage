11409a7290b1912a14f5d6a4ffacf17f512004da
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-09 15:08:12 +0300

    productstotal function added

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-const productRows = document.querySelector("#product-rowlari");
-const productsSum = document.querySelector(".productstoplam");
-const vergi = document.querySelector(".vergi");
-const kargo = document.querySelector(".kargo");
-const toplam = document.querySelector(".toplam");
+const shipping = 15.0;
+const tax = 0.18;
 
 let sepettekiler = [
   { name: "Vintage Backpack", price: 34.99, piece: 1, img: "./img/photo1.png" },
@@ -19,33 +16,44 @@ sepettekiler.forEach(({ img, name, price, piece }) => {
     <div class="col-md-5 ">
       <img src=${img}  class=" w-100 rounded-start" alt="...">
     </div>
+
     <div class="col-md-7 ">
+
       <div class="card-body">
+      
         <h5 class="card-title">${name}</h5>
-            <div class="ürün-price">
-                <p class="text-warning h2">$
-                    <span class="indirim-price">${(price * 0.7).toFixed(
-                      2
-                    )}</span>
-                    <span class="h5 text-dark text-decoration-line-through">${price}</span>
-                </p>
-            </div>    
-            <div class="border border-1 border-dark shadow-lg d-flex justify-content-center p-2">
-                <div class="adet-controller ">
-                    <button class="btn btn-secondary btn-sm minus">
+        
+             <div class="ürün-price">
+                    <p class="text-warning h2">$
+                      <span class="indirim-price">${(price * 0.7).toFixed(
+                        2
+                      )} </span>
+                      <span class="h5 text-dark text-decoration-line-through">${price}</span>
+                    </p>
+                  </div>
+
+                  
+                  <div
+                    class="border border-1 border-dark shadow-lg d-flex justify-content-center p-2"
+                  >
+                    <div class="adet-controller ">
+                      <button class="btn btn-secondary btn-sm minus">
                         <i class="fas fa-minus"></i>
-                    </button>
-                    <p class="d-inline mx-4" id="ürün-adet">${piece}</p>
-                    <button class="btn btn-secondary btn-sm plus">
+                      </button>
+                      <p class="d-inline mx-4" id="ürün-adet">${piece}</p>
+                      <button class="btn btn-secondary btn-sm plus">
                         <i class="fas fa-plus"></i>
-                    </button>
-                </div>
-                </div>
+                      </button>
+                    </div>
+
+                  </div>
+
                   <div class="ürün-removal mt-4">
                     <button class="btn btn-danger btn-sm w-100 remove-product">
                       <i class="fa-solid fa-trash-can me-2"></i>Remove
                     </button>
                   </div>
+
                   <div class="mt-2">
                     Ürün Toplam: $<span class="product-total">${(
                       price *
@@ -53,9 +61,30 @@ sepettekiler.forEach(({ img, name, price, piece }) => {
                       piece
                     ).toFixed(2)} </span>
                   </div>
-                </div>
-            </div>
+      </div>
     </div>
+  </div>
 </div>
     `;
 });
+
+producstTotal();
+
+function producstTotal() {
+  const toplam = document.querySelectorAll(".product-total");
+
+  const pToplam = Array.from(toplam).reduce(
+    (acc, item) => acc + Number(item.textContent),
+    0
+  );
+
+  document.querySelector(".productstoplam").textContent = pToplam;
+
+  document.querySelector(".vergi").textContent = (pToplam * tax).toFixed(2);
+
+  document.querySelector(".kargo").textContent = pToplam ? shipping : 0;
+
+  document.querySelector(".toplam").textContent = pToplam
+    ? (pToplam + pToplam * tax + shipping).toFixed(2)
+    : 0;
+}
